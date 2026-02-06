5bef0c10d52b1f70923348881140b148b487d9e2
utssv21 <118469502+utssv21@users.noreply.github.com>
2026-02-06 15:18:10 +0530

    Update server port to use environment variable

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -19,10 +19,11 @@ app.get("/",(req,res)=>{
 });
 
 
-const port=5000;
+const port=process.env.PORT || 5000;
 
 
 
 app.listen(port,()=>{
     console.log(`server is running on port ${port}`);
+
 });
